844ce208ff18ee858cef4cef9c1226d4ed747d6c
Raamish aziz <raamish96@gmail.com>
2023-05-15 19:02:12 +0500

    util change

--- src/crypto/utils/Constants.java
+++ src/crypto/utils/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 	public static String driver = "com.mysql.jdbc.Driver";
 	public static String url = "jdbc:mysql://localhost:3306/workDB";
 	public static String user = "root";
-	public static String password = "gunnerforlife007";
+	public static String password = "root";
 	
 }
